62d1d27f0821fbec3f3dd3240041bafdfe1b1f20
Daniel Siegenthaler <daniel.siegenthaler.1@gmail.com>
2017-05-07 09:13:29 +0200

    Some corrections

--- Windows_Server_with_static_IP_UEFI/Create_Unattended_ISO.ps1
+++ Windows_Server_with_static_IP_UEFI/Create_Unattended_ISO.ps1
@@ -18,23 +18,26 @@ Param(
         [Parameter(Mandatory=$False)]
         [string]$autounattend_File_Path,
 
-        [Parameter(Mandatory=$False)]
+        [Parameter(Mandatory=$true)]
         [string]$Servername,
 
-        [Parameter(Mandatory=$False)]
+        [Parameter(Mandatory=$true)]
         [string]$IP,
 
-        [Parameter(Mandatory=$False)]
+        [Parameter(Mandatory=$true)]
         [string]$Gateway,
 
-        [Parameter(Mandatory=$False)]
+        [Parameter(Mandatory=$true)]
         [string]$Subnet,
 
-        [Parameter(Mandatory=$False)]
-        [string]$Suffix,
+        [Parameter(Mandatory=$false)]
+        [string]$Domain_Suffix,
 
-        [Parameter(Mandatory=$False)]
+        [Parameter(Mandatory=$true)]
         [string]$DNS,
+
+        [Parameter(Mandatory=$false)]
+        [string]$Path_to_Oscdimg,
 
         [Parameter(Mandatory=$False)]
         [ValidateSet('True','False')]
@@ -47,40 +50,40 @@ $StopWatch.Start()
 
 #Fixed Vars
 #Folders for the temp files
-if($Temp_Folder_Data.Length -lt 1) 
+If($Temp_Folder_Data.Length -lt 1) 
 {
     $Temp_Folder_Data = "C:\Temp\ISO"
 }
-if($Temp_Folder_WIM.Length -lt 1)
+If($Temp_Folder_WIM.Length -lt 1)
 {
     $Temp_Folder_WIM = "C:\Temp\WIM"
 }
 
 #path to the image
-if($Image_Path.Length -lt 1) 
+If($Image_Path.Length -lt 1) 
 {
     $Image_Path = "C:\Temp\Original_ISO\14393.0.160715-1616.RS1_RELEASE_SERVER_EVAL_X64FRE_EN-US.ISO"    
 }
 
 #Path to the prepared autoattend.xml
-if($autounattend_File_Path.Length -lt 1)
+If($autounattend_File_Path.Length -lt 1)
 {
     $autounattend_File_Path = "C:\Temp\Answer_File\autounattend.xml"
 }
 
 #Servername
-if($Servername.Length -lt 1)
+If($Servername.Length -lt 1)
 {
     $Servername = "Testserver"
 }
 
 #Networking
-if ($IP.Length -lt 1) 
+If($IP.Length -lt 1) 
 {
     $IP = "192.168.0.111"    
 }
 
-if ($Gateway.Length -lt 1) 
+If($Gateway.Length -lt 1) 
 {
     $Gateway = "192.168.0.1"
 }
@@ -90,21 +93,25 @@ If($Subnet.Length -lt 1)
     $Subnet = "255.255.255.0"
 }
 
-if($Suffix.Length -lt 1)
+If($Domain_Suffix.Length -lt 1)
 {
-    $Suffix = "domain.internal"
+    $Domain_Suffix = "domain.internal"
 }
-if ($DNS.Length -lt 1) 
+If($DNS.Length -lt 1) 
 {
     $DNS = "192.168.0.1"
 }
+if($Path_to_Oscdimg.Length -lt 1)
+{
+    $Path_to_Oscdimg = "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg"
+}
 
 #Debug on/off"
-if($DebugMode -eq $true)
+If($DebugMode -eq $true)
 {
     $DebugPreference = "Continue"
 }
-else 
+Else 
 {
     $DebugPreference = "SilentlyContinue"
 }
@@ -117,7 +124,7 @@ Function Mount-StandardISO
         [string]$Temp_Folder_WIM,
         [string]$Temp_Folder_Data
     )
-    if(!(Test-path $Image_Path))
+    If(!(Test-path $Image_Path))
     {
         Write-Debug "ISO not found"
         Exit
@@ -126,7 +133,7 @@ Function Mount-StandardISO
     $driveLetter = ($Mountdrive | Get-Volume).DriveLetter
     Write-Debug "ISO mounted with driveletter $driveletter"
     
-        if (!(Test-Path $Temp_Folder_WIM)) 
+        If(!(Test-Path $Temp_Folder_WIM)) 
         {
             New-Item -Path $Temp_Folder_WIM -ItemType Directory
             write-debug "Folder temp wim created"
@@ -174,9 +181,11 @@ Function Replace_Placeholders
     # Parameter help description
     [CmdletBinding()]
     Param(
-    [Parameter(Mandatory = $false)]
+    [Parameter(Mandatory = $true)]
     [string]$OldValue,
+    [Parameter(Mandatory = $true)]
     [string]$NewValue,
+    [Parameter(Mandatory = $true)]
     [string]$Temp_Folder_Data
     )
 
@@ -191,18 +200,25 @@ Function Modify_autounattend_File
     # Parameter help description
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $true)]
         [string]$Servername,
+        [Parameter(Mandatory = $true)]
         [string]$IP,
+        [Parameter(Mandatory = $true)]
         [string]$Gateway,
+        [Parameter(Mandatory = $true)]
         [string]$Subnet,
+        [Parameter(Mandatory = $true)]
         [string]$File_Path,
+        [Parameter(Mandatory = $true)]
         [string]$DNS,
-        [string]$Suffix,
+        [Parameter(Mandatory = $true)]
+        [string]$Domain_Suffix,
+        [Parameter(Mandatory = $true)]
         [string]$Temp_Folder_Data
     )
 
-        if ($Subnet -eq "255.255.255.0") 
+        If($Subnet -eq "255.255.255.0") 
         {
             $SubnetSegment = "/24"    
         }
@@ -228,14 +244,15 @@ Function Modify_autounattend_File
     Replace_Placeholders -OldValue "%Gateway%" -NewValue $Gateway -Temp_Folder_Data $Temp_Folder_Data
     Replace_Placeholders -OldValue "%Computername%" -NewValue $Servername -Temp_Folder_Data $Temp_Folder_Data
     Replace_Placeholders -OldValue "%DNS1%" -NewValue $DNS -Temp_Folder_Data $Temp_Folder_Data
-    Replace_Placeholders -OldValue "%Suffix%" -NewValue $Suffix -Temp_Folder_Data $Temp_Folder_Data
+    Replace_Placeholders -OldValue "%Suffix%" -NewValue $Domain_Suffix -Temp_Folder_Data $Temp_Folder_Data
 }
 
 Function Copy-InstallWIM
 {
     Param(
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $true)]
         [string]$Temp_Folder_WIM,
+        [Parameter(Mandatory = $true)]
         [string]$Temp_Folder_Data
     )  
         try 
@@ -255,18 +272,20 @@ Function CreateISO
     # Parameter help description
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory = $false)]
-        [string]$ISO_Name
+        [Parameter(Mandatory = $true)]
+        [string]$ISO_Name,
+        [Parameter(Mandatory = $true)]
+        [string]$Path_to_Oscdimg
     )
     
-    $Path_to_Oscdimg = "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg"
+    #$Path_to_Oscdimg = "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg"
     Push-Location -Path $Path_to_Oscdimg
     try 
     {
         .\oscdimg.exe -bC:\Temp\ISO\efi\microsoft\boot\efisys_noprompt.bin -u2 -h -m -lWIN_SERVER_2016 C:\Temp\ISO C:\Temp\$ISO_Name
         If($LASTEXITCODE -eq 0)
         {
-            Write-Debug "ISO succesfull created"
+            Write-Debug "Iso succesfull created"
         }
         else 
         {
@@ -275,7 +294,7 @@ Function CreateISO
     }
     catch 
     {
-        Write-Debug "ISO creation failed"
+        Write-Debug "Iso creation failed"
         Exit 1
     }
     Pop-Location
@@ -286,8 +305,9 @@ Function Cleanup
     # Parameter help description
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $true)]
         [string]$Temp_Folder_Data,
+        [Parameter(Mandatory = $true)]
         [string]$Temp_Folder_WIM
     )
 
@@ -301,13 +321,13 @@ Function Cleanup
 Mount-StandardISO -Image_Path $Image_Path -Temp_Folder_Data $Temp_Folder_Data -Temp_Folder_WIM $Temp_Folder_WIM
 
 #Modify unattended.xml
-Modify_autounattend_File -Servername $Servername -File_Path $autounattend_File_Path -Temp_Folder_Data $Temp_Folder_Data -IP $IP -Gateway $Gateway -Subnet $Subnet -DNS $DNS -Suffix $Suffix
+Modify_autounattend_File -Servername $Servername -File_Path $autounattend_File_Path -Temp_Folder_Data $Temp_Folder_Data -IP $IP -Gateway $Gateway -Subnet $Subnet -DNS $DNS -Suffix $Domain_Suffix
 
 #Copy install.wim to ISO directory
 Copy-InstallWIM -Temp_Folder_WIM $Temp_Folder_WIM -Temp_Folder_Data $Temp_Folder_Data
 
 #Create Boot ISO combined with unattendend.xml
-CreateISO -ISO_Name "Windows_Server_2016_Datacenter_$Servername.iso"
+CreateISO -ISO_Name "Windows_Server_2016_Datacenter_$Servername.iso" -Path_to_Oscdimg $Path_to_Oscdimg
 
 #Delete temp folders and files
 Cleanup -Temp_Folder_Data $Temp_Folder_Data -Temp_Folder_WIM $Temp_Folder_WIM
